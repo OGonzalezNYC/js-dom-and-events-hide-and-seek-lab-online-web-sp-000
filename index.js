9209b7434911968403c607ba35353f87a7feb1a3
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-12-30 02:35:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,27 +2,39 @@ function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
-function nestedTarget() {
-  return document.querySelector('#nested .target');
+
+function nestedTarget(nested, target) {
+  return document.querySelector( '#nested .target' )
+  // let nest = document.getElementById('nested')
+  // return nest.getElementsByClassName('target') RETURNS AN ARRAY, AS INDICATED BY THE PLURAL "Elements"
 }
 
+
 function increaseRankBy(n) {
-  let allRankedLists = document.querySelectorAll('.ranked-list li');
-  for (let i = 0; i < allRankedLists.length; i++) {allRankedLists[i].innerHTML = parseInt(allRankedLists[i].innerHTML) + n;
+  let x = document.getElementsByClassName('ranked-list')
+  for (let i = 0; i < x.length; i++) {
+    x[i].innerHTML = (parseInt(x[i].innerHTML) + n)
   }
+  // return x
 }
+
+// function increaseRankBy(n) {
+//   let allRankedLists = document.querySelectorAll('.ranked-list li');
+//   for (let i = 0; i < allRankedLists.length; i++) {allRankedLists[i].innerHTML = parseInt(allRankedLists[i].innerHTML) + n;
+//   }
+// }
 
 
 function deepestChild() {
-  let grandNode = document.querySelector('#grand-node');
-  
-  // let grandNodeDescendants = grandNode.querySelectorAll('div');
-  // return grandNodeDescendants[grandNodeDescendants.length-1]
-  
-  let child = grandNode.children[ 0 ];
-  while ( child ) {
-    grandNode = child;
-    child = grandNode.children[ 0 ];
-  }
-  return grandNode;
+  return document.querySelector('div#grand-node').children[0].children[0].children[0].children[0]
 }
+
+// function deepestChild() {
+//   let grandNode = document.querySelector('#grand-node');
+//   let child = grandNode.children[ 0 ];
+//   while ( child ) {
+//     grandNode = child;
+//     child = grandNode.children[ 0 ];
+//   }
+//   return grandNode;
+// }
